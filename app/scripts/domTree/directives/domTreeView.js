7143b10a9061091c9858d92d6c5f0d61891ee402
Ivan Demchenko <raqy.style@gmail.com>
2015-04-18 17:58:30 +0200

    Removed Recursive helper from domTree directive

--- app/scripts/domTree/directives/domTreeView.js
+++ app/scripts/domTree/directives/domTreeView.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('uiBuilderApp.domTree')
-  .directive('domTreeView', function(RecursionHelper, $rootScope, ElemManager, DomTreeParser, DragDropHandler) {
+  .directive('domTreeView', function($rootScope, ElemManager, DomTreeParser, DragDropHandler) {
     return {
       restrict: 'E',
       replace: true,
@@ -9,26 +9,24 @@ angular.module('uiBuilderApp.domTree')
         tree: '='
       },
       templateUrl: 'scripts/domTree/directives/domTreeView.html',
-      compile: function(elem) {
-        return RecursionHelper.compile(elem, function(scope) {
-          DragDropHandler.bindEventHandlers(elem[0]);
+      link: function(scope, elem) {
+        DragDropHandler.bindEventHandlers(elem[0]);
 
-          // FIXME: Recursive event binding
-          $rootScope.$on('uib:canvas:updated', function(evt, root) {
-            evt.stopPropagation();
-            scope.$apply(function() {
-              scope.tree = DomTreeParser.buildTree(root);
-            });
+        // FIXME: Recursive event binding
+        $rootScope.$on('uib:canvas:updated', function(evt, root) {
+          evt.stopPropagation();
+          scope.$apply(function() {
+            scope.tree = DomTreeParser.buildTree(root);
           });
-
-          scope.editElem = function(node) {
-            ElemManager.startEditElem(node.domElem);
-          };
-
-          scope.removeElem = function(node) {
-            ElemManager.removeElem(node.domElem);
-          };
         });
+
+        scope.editElem = function(node) {
+          ElemManager.startEditElem(node.domElem);
+        };
+
+        scope.removeElem = function(node) {
+          ElemManager.removeElem(node.domElem);
+        };
       }
     };
   });
