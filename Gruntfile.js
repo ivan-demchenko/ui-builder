cf3d1513edcf091e8fa1edb4cb316a45f39ba965
Ivan Demchenko <raqy.style@gmail.com>
2015-04-18 21:25:56 +0200

    cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -207,8 +207,7 @@ module.exports = function(grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/**/*.js',
           '<%= yeoman.dist %>/styles/**/*.css',
-          '<%= yeoman.dist %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%= yeoman.dist %>/styles/fonts/*'
+          '<%= yeoman.dist %>/font/*'
         ]
       }
     },
@@ -239,7 +238,6 @@ module.exports = function(grunt) {
       options: {
         assetsDirs: [
           '<%= yeoman.dist %>',
-          '<%= yeoman.dist %>/images',
           '<%= yeoman.dist %>/styles'
         ]
       }
